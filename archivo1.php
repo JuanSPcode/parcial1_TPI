df265add044e7eaf4a15fe9af12dbcc58c597773
Juan Sandoval <js3729395@gmail.com>
2025-09-04 12:47:05 -0600

    add: array de personajes del ejercicicio 1

--- archivo1.php
+++ archivo1.php
@@ -1,3 +1,21 @@
 <?php
- echo "hola";
+    $personajes[] = [["id" => 'img1', "nombre" => "Lord FarkCua","descripcion" => 'Es un señor que se quiso casar con la novia del prota', "categoria" => 'Villano'],
+                    ["id" => 'img2', "nombre" => "Papucho","descripcion" => 'Es la novia de Santos', "categoria" => 'Extra'],
+                    ["id" => 'img3', "nombre" => "Shrek","descripcion" => 'Es el protagonista que sale', "categoria" => 'Protagonista'],
+                    ["id" => 'img4', "nombre" => "Burro","descripcion" => 'Es el amigo del protagonista', "categoria" => 'Protagonista'],
+                    ["id" => 'img5', "nombre" => "Jenjibre","descripcion" => 'el amigo galleta del protagonista', "categoria" => 'Extra']];
+
+    var_dump($personajes);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shrek</title>
+</head>
+<body>
+    
+</body>
+</html>
